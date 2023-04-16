f52935dfb139ecee5ae08a0eb26dac68e65a9f24
Roman Radionov <roman.s.radionov@gmail.com>
2023-04-16 23:25:23 +0300

    add card

--- src/componets/Card/card.js
+++ src/componets/Card/card.js
@@ -3,14 +3,17 @@ import React from 'react';
 import {ourStudy} from '../../data';
 
 const Card = () => {
+    console.log(ourStudy.map(item => item.lessons[0]))
     return (
-        <div>
+        <>
             <ul>
                 {ourStudy.map(item => (
-                    <li key={item.id}>{item.id}</li>
+                    <li key={item.id}>
+                        <div>id:{item.id} - {item.title}</div>
+                    </li>
                 ))}
             </ul>
-        </div>
+        </>
     );
 };
 
