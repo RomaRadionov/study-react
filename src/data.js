9a50813edbd3bc85d9f64e3c05ab328f1f61ab93
Roman Radionov <roman.s.radionov@gmail.com>
2023-04-16 16:56:00 +0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,247 @@
+let ourStudy = [
+    {
+        id: 1,
+        title: 'GIT',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'git',
+                video: 'https://www.youtube.com/watch?v=g41rds_1k3E'
+            },
+            {
+                lesson: 2,
+                title: 'git',
+                video: 'https://www.youtube.com/watch?v=WPHDGsifCnM'
+            }
+        ],
+    },
+    {
+        id: 2,
+        title: 'SCRUM',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'scrum',
+                video: 'https://www.youtube.com/watch?v=4gaTlViKpX8'
+            },
+            {
+                lesson: 2,
+                title: 'scrum',
+                video: 'https://www.youtube.com/watch?v=emYV_nR9q1w'
+            },
+            {
+                lesson: 3,
+                title: 'scrum',
+                video: 'https://www.youtube.com/watch?v=S6b4NozenOw'
+            },
+            {
+                lesson: 4,
+                title: 'scrum',
+                video: 'https://www.youtube.com/watch?v=4bFc57z0KRU'
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: 'HTML&CSS',
+        preview: 'https://www.youtube.com/playlist?list=PLY1sAemBLA5w_U2bqiu8UOQwH1KgWW0CO',
+        consultation: 'https://www.youtube.com/watch?v=zAhh0kndBR4',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'base structure',
+                video: 'https://www.youtube.com/watch?v=VY5R3VLDJSM',
+                resolve: 'https://www.youtube.com/watch?v=JeDKL6AJW8k',
+            },
+            {
+                lesson: 2,
+                part: 1,
+                title: 'box model',
+                video: 'https://www.youtube.com/watch?v=JUMice64D1Q',
+            },
+            {
+                lesson: 2,
+                part: 2,
+                title: 'Flex',
+                video: 'https://www.youtube.com/watch?v=c18jsdx-_wk',
+                resolve: 'https://www.youtube.com/watch?v=Z-1nJjOhHP4',
+            },
+            {
+                lesson: 3,
+                title: 'selectors positions',
+                video: 'https://www.youtube.com/watch?v=mUjrdQB088w',
+                resolve: 'https://www.youtube.com/watch?v=vY-BspoUOpo',
+            },
+            {
+                lesson: 4,
+                title: 'media bootstrap',
+                video: 'https://www.youtube.com/watch?v=iiwLUjBx3oE',
+                resolve: 'https://www.youtube.com/watch?v=R85CSbZwccs',
+            },
+
+        ],
+    },
+    {
+        id: 4,
+        title: 'JAVASCRIPT',
+        preview: 'https://www.youtube.com/playlist?list=PLY1sAemBLA5zOMdo9YmA39i5g1VgVUbQN',
+        consultation: ['https://www.youtube.com/watch?v=9dhNsVl7Pgc',
+                        'https://www.youtube.com/watch?v=UccgNGgGREE',
+                        'https://www.youtube.com/watch?v=wW4gDGX6VUs',
+                        'https://www.youtube.com/watch?v=iJOWzSj6F_I',
+                        'https://www.youtube.com/watch?v=4OTvd-9jWNg',
+                        'https://www.youtube.com/watch?v=NJDR_0efBkk',
+                        'https://www.youtube.com/watch?v=V2J2foiY134'],
+        lessons: [
+            {
+                lesson: 1,
+                title: 'vars types',
+                video: 'https://www.youtube.com/watch?v=QSwUGCR3zbg',
+                resolve: 'https://www.youtube.com/watch?v=hATrSK1XXYM',
+            },
+            {
+                lesson: 2,
+                title: 'arrays objects if switch',
+                video: 'https://www.youtube.com/watch?v=GRhGEKxi9AI',
+                resolve: 'https://www.youtube.com/watch?v=XHEMQHmYT5E',
+            },
+            {
+                lesson: 3,
+                title: 'loops',
+                video: 'https://www.youtube.com/watch?v=ffOzKtWqFvo',
+                resolve: 'https://www.youtube.com/watch?v=B6lgXB3SHjQ',
+            },
+            {
+                lesson: 4,
+                title: 'functions',
+                video: 'https://www.youtube.com/watch?v=IDo-4Szr2Aw',
+                resolve: 'https://www.youtube.com/watch?v=jhBmrsp35hY',
+            },
+            {
+                lesson: 5,
+                title: 'functions recursion callbacks',
+                video: 'https://www.youtube.com/watch?v=a3vlu15363I',
+                resolve: 'https://www.youtube.com/watch?v=JqdeJu8NBGw',
+                additionalVideo: 'https://www.youtube.com/watch?v=bmvw0QpQC54',
+            },
+            {
+                lesson: 6,
+                title: 'array-methods',
+                video: 'https://www.youtube.com/watch?v=oxNCCu8XlCo',
+            },
+            {
+                lesson: 7,
+                title: 'es6',
+                video: 'https://www.youtube.com/watch?v=XZYwgNuVLMY',
+            },
+            {
+                lesson: 8,
+                title: 'constructors',
+                video: 'https://www.youtube.com/watch?v=55cjiDcYMUQ',
+                resolve: 'https://www.youtube.com/watch?v=JbqKhxrJP4A',
+            },
+            {
+                lesson: 9,
+                title: 'DOM',
+                video: 'https://www.youtube.com/watch?v=l3iaHWjYzzY',
+                resolve: 'https://www.youtube.com/watch?v=F90QDTuzZHU',
+            },
+            {
+                lesson: 10,
+                title: 'events storage',
+                video: 'https://www.youtube.com/watch?v=AIxXYDxN9yo',
+                resolve: 'https://www.youtube.com/watch?v=N105R6WDcos',
+            },
+            {
+                lesson: 11,
+                title: 'async',
+                video: 'https://www.youtube.com/watch?v=NUhASlkCdOc',
+            },
+            {
+                lesson: 12,
+                title: 'url http regexp',
+                video: 'https://www.youtube.com/watch?v=EowaX3iFUJA',
+                resolve: 'https://www.youtube.com/watch?v=N105R6WDcos',
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: 'MYSQL',
+        link: 'http://owu.linkpc.net',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'MYSQL',
+                video: 'https://www.youtube.com/watch?v=muH1aRpHMJw',
+            },
+            {
+                lesson: 2,
+                title: 'MYSQL',
+                video: 'https://www.youtube.com/watch?v=55uJQPD86mg',
+            },
+            {
+                lesson: 3,
+                title: 'MYSQL',
+                video: 'https://www.youtube.com/watch?v=Nnqq1J1B0vM',
+            }
+        ],
+    },
+    {
+        id: 6,
+        title: 'MONGODB',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'MONGODB',
+                video: 'https://www.youtube.com/watch?v=v2R5deW8j5A',
+            },
+            {
+                lesson: 2,
+                title: 'MONGODB',
+                video: 'https://www.youtube.com/watch?v=WoU6TT3FRYU',
+            },
+            {
+                lesson: 3,
+                title: 'MONGODB',
+                video: 'https://www.youtube.com/watch?v=yYYlvhkiuGY',
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: 'TYPESCRIPT',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'TYPESCRIPT',
+                video: 'https://www.youtube.com/watch?v=4eUYkBlPITw',
+                resolve: 'https://www.youtube.com/watch?v=2R96oB8tAJI',
+            },
+        ],
+    },
+    {
+        id: 8,
+        title: 'REACT',
+        preview: 'https://www.youtube.com/playlist?list=PLY1sAemBLA5xI4du0481aeHnjybZD__e7',
+        lessons: [
+            {
+                lesson: 1,
+                title: 'REACT',
+                video: 'https://www.youtube.com/watch?v=xB5hjv6oaXs',
+                resolve: 'https://www.youtube.com/watch?v=ziZn30FQemc',
+            },
+            {
+                lesson: 2,
+                title: 'REACT',
+                video: 'https://www.youtube.com/watch?v=FsjWBHEuU-k',
+                resolve: 'https://www.youtube.com/watch?v=dAw88KDos8E',
+            },
+            {
+                lesson: 3,
+                title: 'REACT',
+                video: 'https://www.youtube.com/watch?v=_lExN2FDZxU',
+            },
+        ],
+    }
+];
